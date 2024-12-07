f799aac4246aaadd5a0be2a53667553517d98411
bharaths1803 <bharaths14051803@gmail.com>
2024-12-07 14:02:45 +0530

    Completed full implementation with socketio

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
+
 
 export const sendMessage = async (req, res) => {
   try{
@@ -28,6 +30,12 @@ export const sendMessage = async (req, res) => {
     }
 
     //Socket io implementations
+
+    const receiverSocketId = getReceiverSocketId(receiverId)
+    if(receiverSocketId){
+      io.to(receiverSocketId).emit("newMessage", newMessage)
+    }
+
 
     //Parallel processing
     await Promise.all([conversation.save(), newMessage.save()]);
--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,10 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -22,7 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
   connectToMongoDB();
 })
